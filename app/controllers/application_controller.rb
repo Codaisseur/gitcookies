a24c65e4c7953a4c76668621dc8ad38b84c1b582
Wouter de Vos <wouter@codaisseur.com>
2016-06-10 17:04:27 +0200

    Store favorite cookie in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  #protect_from_forgery with: :exception
+
+  def cookie
+  end
+
+  def set_cookie
+    session[:favorite_cookie] = params[:favorite_cookie]
+    redirect_to action: :cookie
+  end
 end
